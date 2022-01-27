ede346efe31c03c883ea115157934ff41401f47b
Mitch <mitchodongo@gmail.com>
2022-01-27 13:05:16 +0300

    updating post list ui and place holders

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="row">
+            <div class="col-10">                
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Dashboard') }}
+                </h2>
+            </div>
+            <div class="col-2">                
+                <a href="{{ route('posts.create') }}" class="btn btn-dark bg-dark pull-right">
+                    New Post
+                </a>
+            </div>
+        </div>
     </x-slot>
-    <a href="{{ route('posts.create') }}">
-        New Post
-    </a>
+    
     {{-- Prompt if no posts found --}}
     @if ($posts->count()==0)
         
@@ -26,8 +33,20 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     @foreach ($posts as $post)
-                        <div class="p-6 bg-white border-b border-gray-200">
-                            {{$post->title}}
+                        <div class="p-6 bg-white border-b border-gray-200 row">
+                            <div class="row">
+                                <div class="col-1">                                    
+                                    @if(!empty($post->image))
+                                        <img  src="{{asset( $post->image )}}" alt="{{$post->image}}" class="img-responsive"/>
+                                    @else
+                                        <img  src="/images/placeholder.png" alt="{{$post->image}}" class="img-responsive"/>
+                                    @endif
+                                </div>
+                                <div class="col-6">                                    
+                                    {{$post->title}}
+                                </div>
+                                <div class="col-2"></div>
+                            </div>
                         </div>
                     @endforeach                    
                 </div>
